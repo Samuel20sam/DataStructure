04655b3e5a30fad6c83d84941c948666daf882d6
Samuel_Elijah <samuel20sam@gmail.com>
2022-06-26 22:28:14 +0530

    Adding cc package

---
+++ src/com/bridgelabz/cc/CashCounter.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.cc;
+
+public class CashCounter {
+    Person head;
+    static int noOfCustomers;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    private void addLast(int money) {
+        Person person = new Person(money);
+        noOfCustomers--;
+        if (isEmpty()) {
+            head = person;
+            head.bankBalance += head.money;
+            noOfCustomers++;
+            return;
+        }
+
+        Person currPerson = head;
+        while (currPerson.nextPerson != null) {
+            currPerson = currPerson.nextPerson;
+        }
+        currPerson.nextPerson = new Person(money);
+        currPerson.nextPerson.bankBalance += currPerson.nextPerson.money;
+    }
+
+    public void enqueue(int money) {
+        addLast(money);
+    }
+
+    private void deleteFirst() {
+        if (isEmpty()) {
+            return;
+        }
+        head = head.nextPerson;
+        noOfCustomers--;
+    }
+
+    public void dequeue() {
+        deleteFirst();
+    }
+
+    public int noOfPersonInQueue() {
+        System.out.println();
+        return noOfCustomers;
+    }
+}
